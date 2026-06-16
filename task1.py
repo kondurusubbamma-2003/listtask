8f7665b254a1c2c1aee22e82575c68647995c456
kondurusubbamma-2003 <kondurumaidhili@gmail.com>
2026-06-16 18:58:40 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,24 @@
+names=["nellore","gudur","tirupati","kavali","hyderabad",]
+print(names)
+
+print(names[2])
+
+print(names[0:3])
+
+names.sort()
+print(names)
+
+names.append("banglore")
+print(names)
+
+names.remove("nellore")
+print(names)
+
+def city_length(names):
+    return len(names)
+
+
+print(city_length(names))
+
+
+
